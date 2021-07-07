fda0681b50007422ec334c1ffd0c2bac9a73fbe0
EliasSalom <elias.29@hotmail.com>
2021-07-07 16:13:46 +0300

    fix server issue port number

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ io.on('connection', socket => {
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => {
     console.log("Server is listening on localhost::" + PORT);
 });
